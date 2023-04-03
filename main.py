8aa8274c5086e8062aab69605f3d4daa3516fa2a
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-04-03 17:58:41 +0100

    Add individual blog post pages

--- main.py
+++ main.py
@@ -22,5 +22,20 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route('/<post_title>')
+def post(post_title, blog_json=response):
+
+    def get_post_data(blog_json, post_title):
+        post_result = []
+        for entry in blog_json:
+            if post_title in entry.values():
+                post_result.append(entry)
+        return post_result[0]
+
+    blog_entry = get_post_data(blog_json, post_title)
+
+    return render_template("post.html", post=blog_entry)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
